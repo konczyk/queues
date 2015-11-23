fa0ef8016cf73402042c3d535153329e7d6c3f66
Daniel Kończyk <daniel@konczyk.net>
2015-11-23 05:19:17 +0100

    Add Deque API skeleton

---
+++ src/Deque.java
@@ -0,0 +1,41 @@
+import java.util.Iterator;
+
+// A double-ended queue
+public class Deque<T> implements Iterable<T> {
+
+    public Deque() {
+    }
+
+    // check if deque is empty
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return number of items in the deque
+    public int size() {
+        return 0;
+    }
+
+    // add item at the front of the deque
+    public void addFirst(T item) {
+    }
+
+    // add item at the tail of the deque
+    public void addLast(T item) {
+    }
+
+    // remove and return head element
+    public T removeFirst() {
+        return null;
+    }
+
+    // remove and return tail element
+    public T removeLast() {
+        return null;
+    }
+
+    // return iterator over elements from head to tail
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
