bd6137ff0c374c613b76f0882dac07d17136b94e
Daniel Kończyk <daniel@konczyk.net>
2015-12-21 20:20:09 +0100

    Add RandomizedQueue API

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    public RandomizedQueue() {
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    // add item to the queue
+    public void enqueue(Item item) {
+    }
+
+    // remove and return a random item from the queue
+    public Item dequeue() {
+        return null;
+    }
+
+    // return a random item from the queue
+    public Item sample() {
+        return null;
+    }
+
+    // return an iterator over items in random order
+    public Iterator<Item> iterator() {
+        return null;
+    }
+}
